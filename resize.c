d09d028c44e16ea4ce11c628b7c64e35a3f2f1e4
Ishaan Mishra <ishaan1101@gmail.com>
2017-05-16 14:25:56 +0530

    Addin BITMAPINFOHEADER and BITFILEHEADER

--- resize.c
+++ resize.c
@@ -56,3 +56,18 @@ int main(int argc, char* argv[])
     // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+
+// read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is  a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
